64add440a9300e96c1d513ce51a2795c149ee247
ForestParadiseOrigen <andres.jcv.678@gmail.com>
2023-12-04 07:44:01 -0500

    forgotPassword

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
@@ -5,7 +6,8 @@ import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
   {path:'', component:LoginComponent},
-  {path:'register', component:RegisterComponent}
+  {path:'register', component:RegisterComponent},
+  {path:'forgotPassword', component:ForgotPasswordComponent}
 
 ];
 
